3d0a5e163f1104bc10ad17550819cdeebbe4ae2e
Admin <Admin@PRISM05>
2019-05-22 15:18:04 +0530

    Added screenshot reporting when test is in  Headlessmode

--- Inv_Login/src/main/java/com/inv/Login/LoginTest.java
+++ Inv_Login/src/main/java/com/inv/Login/LoginTest.java
@@ -1,10 +1,14 @@
 package com.inv.Login;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,16 +18,17 @@ public class LoginTest {
 	protected WebDriver driver;
 	protected Properties prop = new Properties();
 	// private static WebDriver driver;
-             
+
 	@Test(priority = 1)
-	public void Loginvalidation() throws IOException {
+	public void Loginvalidation() throws IOException, Exception {
 
 		System.setProperty("webdriver.chrome.driver", "C://Users//Admin//Desktop//chromedriver.exe");
 
-		ChromeOptions options = new ChromeOptions(); 
+		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless");
 		options.addArguments("--disable-gpu");
-		//options.addArguments("window-size=1366,768");
+		
+		options.addArguments("window-size=1366,768");
 		driver = new ChromeDriver(options);
 		// create a object for a property class
 		FileInputStream file = new FileInputStream("D:\\Inv_Login\\src\\main\\java\\com\\inv\\Login\\LoginData");
@@ -34,7 +39,12 @@ public class LoginTest {
 		driver.findElement(By.id(prop.getProperty("Id_id"))).sendKeys(prop.getProperty("loginid"));
 		driver.findElement(By.id(prop.getProperty("Pswd"))).sendKeys(prop.getProperty("password"));
 		driver.findElement(By.id("btnSubmit")).click();
-
+		Thread.sleep(1000);
+		options.addArguments("screenshot");
+		
+		File locatefolder = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(locatefolder, new File("C:\\Users\\Admin\\git\\FirstGit\\Inv_Login\\ss\\invlogin.png"));
+		System.out.println("captured");
 	}
 
 }
